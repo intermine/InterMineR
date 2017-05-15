c1e3f2e8c95a4dcbed552242aea92167e687f799
Konstantinos Kyritsis <konstantinoskyritsis89@gmail.com>
2017-05-15 12:54:18 +0300

    Update InterMineR.R
    Error occuring when using HumanMine:
    The fourth column of the att.ext variable is redundant and will prevent the rbind(att.ext, ref.ext, col.ext) that occurs below. 
    Therefore the line:
    att.ext = att.ext[,1:3]
    is added to remove the fourth column.

--- R/InterMineR.R
+++ R/InterMineR.R
@@ -114,6 +114,16 @@ listModelSummary <- function(model){
     att.ext <- do.call(rbind, att.ext)
     att.ext$child_type <- ""
     rownames(att.ext) <- NULL
+                  
+    # Error occuring when using HumanMine:
+    # The fourth column of the att.ext variable is redundant and will prevent the
+    # rbind(att.ext, ref.ext, col.ext) below!!!
+  
+     # columns 2 and 4 contain identical information
+    all(tolower(att.ext[,2]) %in% gsub(" ", "", tolower(att.ext[,4])))
+  
+    # Therefore, we keep only the first 3 columns from the att.ext variable:
+    att.ext = att.ext[,1:3]
 
     ref <- lapply(class.name, function(x) {
         y <- model[[x]][["references"]]
