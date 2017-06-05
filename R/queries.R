a84f37eca351860a836b24c03d64b06a7e43aa78
Konstantinos Kyritsis <konstantinoskyritsis89@gmail.com>
2017-06-06 01:30:02 +0300

    Update queries.R

--- R/queries.R
+++ R/queries.R
@@ -122,14 +122,3 @@ newQuery <- function(name="", view=character(), sortOrder="", longDescription=""
 
       nq
 }
-
-getWidgets = function(im){
-  # keep the first part of mine url (e.g. http://www.flymine.org/)
-  mine.url = substr(im$mine, start = 1, stop = gregexpr("/",im$mine)[[1]][length(gregexpr("/",im$mine)[[1]])])
-  
-  # get widget results and convert rirectly to R objects with jsonlite::fromJSON
-  res = jsonlite::fromJSON(txt = paste0(mine.url, "query/service/widgets?format=json"))
-  
-  res$widgets
-}
-
