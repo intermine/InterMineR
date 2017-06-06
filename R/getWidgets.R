15ac459ad55e0fa30e91b6480304fae58a8bd46f
Konstantinos Kyritsis <konstantinoskyritsis89@gmail.com>
2017-06-06 13:31:52 +0300

    Update getWidgets.R

--- R/getWidgets.R
+++ R/getWidgets.R
@@ -1,81 +1,90 @@
 getWidgets = function(im){
   
-  # keep the first part of mine url (e.g. http://www.flymine.org/)
-  mine.url = substr(im$mine, start = 1, stop = gregexpr("/",im$mine)[[1]][length(gregexpr("/",im$mine)[[1]])])
+  mine.url = im$mine
   # make GET request
-  r = GET(paste0(mine.url, "query/service/widgets?format=json"))
+  r = GET(paste0(mine.url, "/service/widgets?format=json"))
+  stop_for_status(r)
+  
   # format with content
   mine.script = content(r, "text")
+  
   # RJSONIO::fromJSON instead of jsonlite::fromJSON (interferes with getModel)
+  res = fromJSON(mine.script)
+  
+  # get widget results and convert rirectly to R objects with 
   # res = jsonlite::fromJSON(txt = paste0(mine.url, "query/service/widgets?format=json"))
-  res = RJSONIO::fromJSON(mine.script)
   
   # format to data.frame:
   t = res$widgets
   
-  # get all unique names
-  ind.names = c()
-  for(i in seq(length(t))){
-    ind.names = c(ind.names, names(t[[i]]))
-  }
-  
-  ind.names = unique(ind.names)
-  
-  # empty list
-  l = as.list(seq(length(t)))
-  
-  # iterate through res list
-  for(j in seq(length(t))){
+  if(length(t) == 0){
+    res2 = NULL
+    return(res2)
+  } else {
+    # get all unique names
+    ind.names = c()
+    for(i in seq(length(t))){
+      ind.names = c(ind.names, names(t[[i]]))
+    }
     
-    l.append = c()
+    ind.names = unique(ind.names)
     
-    # if widget has all ind.names
-    if(all(ind.names %in% names(t[[j]]))){
+    # empty list
+    l = as.list(seq(length(t)))
+    
+    # iterate through res list
+    for(j in seq(length(t))){
       
-      for(y in seq(length(ind.names))){
+      l.append = c()
+      
+      # if widget has all ind.names
+      if(all(ind.names %in% names(t[[j]]))){
         
-        n = ind.names[y]
-        item = unlist(t[[j]][n])
-        
-        if(is.null(item)){
-          l.append = c(l.append, NA)
-          names(l.append)[y] = n
-        } else if(length(item) >= 2) {
-          l.append = c(l.append, paste(item, collapse = " & "))
-          names(l.append)[y] = n
-        } else {
-          l.append = c(l.append,item)
-          names(l.append)[y] = n
+        for(y in seq(length(ind.names))){
+          
+          n = ind.names[y]
+          item = unlist(t[[j]][n])
+          
+          if(is.null(item)){
+            l.append = c(l.append, NA)
+            names(l.append)[y] = n
+          } else if(length(item) >= 2) {
+            l.append = c(l.append, paste(item, collapse = " & "))
+            names(l.append)[y] = n
+          } else {
+            l.append = c(l.append,item)
+            names(l.append)[y] = n
+          }
         }
+        
+      } else {
+        
+        for(y in seq(length(ind.names))){
+          
+          n = ind.names[y]
+          item = unlist(t[[j]][n])
+          
+          if(is.null(item)){
+            l.append = c(l.append, NA)
+            names(l.append)[y] = n
+          } else if(length(item) >= 2) {
+            l.append = c(l.append, paste(item, sep = " "))
+            names(l.append)[y] = n
+          } else {
+            l.append = c(l.append,item)
+            names(l.append)[y] = n
+          }
+        }
+        
       }
       
-    } else {
-      
-      for(y in seq(length(ind.names))){
-        
-        n = ind.names[y]
-        item = unlist(t[[j]][n])
-        
-        if(is.null(item)){
-          l.append = c(l.append, NA)
-          names(l.append)[y] = n
-        } else if(length(item) >= 2) {
-          l.append = c(l.append, paste(item, sep = " "))
-          names(l.append)[y] = n
-        } else {
-          l.append = c(l.append,item)
-          names(l.append)[y] = n
-        }
-      }
+      l[[j]] = l.append
       
     }
     
-    l[[j]] = l.append
+    # rbind lists to data.frame
+    res2 = do.call(rbind, l)
     
+    return(res2)
   }
-  
-  # rbind lists to data.frame
-  res2 = do.call(rbind, l)
-  
-  return(res2)
 }
