4ff9362b594615975c2d59e55ab355f2eed585d8
Konstantinos Kyritsis <konstantinoskyritsis89@gmail.com>
2017-07-28 20:22:26 +0300

    Add files via upload

---
+++ R/PL_DiabetesGenes.R
@@ -0,0 +1,20 @@
+#' @title PL_DiabetesGenes data
+#' 
+#' @description A dataset containing identifiers of genes associated with all forms of Diabetes according to OMIM (http://www.omim.org/).
+#' 
+#' @format A dataset containing 68 rows and 6 variables:
+#' \describe{
+#' 
+#' \item{Gene.symbol}{Gene symbol}
+#' \item{Gene.name}{Gene whole name}
+#' \item{Gene.primaryIdentifier}{InterMine Gene.primaryIdentifier (ENTREZ identifier)}
+#' \item{Gene.secondaryIdentifier}{InterMine Gene.secondaryIdentifier (ENSEMBLE identifier)}
+#' \item{Gene.length}{Gene length in base pairs}
+#' \item{Gene.organism.name}{Gene organism name}
+#'  
+#' }
+#' 
+#' @author InterMine Team
+#' 
+#' @source \url{http://www.humanmine.org/humanmine/bag.do?subtab=view}
+"PL_DiabetesGenes"
