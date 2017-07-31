f1f774b00773197abf3259644acdaa88ef905fcc
Konstantinos Kyritsis <konstantinoskyritsis89@gmail.com>
2017-07-31 20:53:37 +0300

    Create summary.R

---
+++ R/summary.R
@@ -0,0 +1,39 @@
+#' @name summary
+#' @rdname summary
+#' @docType methods
+#' @aliases summary summary,ANY-method summary,InterMineR-method
+#' @export
+
+# methods for InterMineR class
+# summary
+
+setGeneric("summary", function(object,...){standardGeneric("summary")})
+
+# set summary method for class InterMineR
+setMethod(
+  "summary",
+  signature(object = "InterMineR"),
+  function(object,...){
+    
+    # create query.log data.frame
+    l = list(NULL)
+    count = 0
+    
+    for(j in seq(length(object@where))){
+      
+      x = object@where[[j]]
+      
+      if("value" %in% names(x)){
+        if(length(x$value) > 1){
+          x$value = paste(x$value, collapse = ",")
+        }
+        count = count + 1
+        l[[count]] = data.frame(x[which(names(x) %in% c("path","op", "value", "code"))])
+        
+      }
+    }
+    
+    return(do.call(rbind,l))
+    
+  }
+)
